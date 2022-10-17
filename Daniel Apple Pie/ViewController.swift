6048253a3bfadbffa95fe15d967a4d1fc4493b79
Daniel <danielbonneau04@gmail.com>
2022-10-17 12:41:03 -0400

    10/17/22

--- Daniel Apple Pie/ViewController.swift
+++ Daniel Apple Pie/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var correctWorldLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var lettterButton: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
 }
 
