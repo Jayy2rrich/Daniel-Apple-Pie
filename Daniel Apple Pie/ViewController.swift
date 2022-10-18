d8aa83c7c6f7f22cb6625176e8717935deda2b0b
Daniel <danielbonneau04@gmail.com>
2022-10-18 14:19:15 -0400

    10/18/22

--- Daniel Apple Pie/ViewController.swift
+++ Daniel Apple Pie/ViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+let incorrectMovesAllowed = 7
+
+var totalWins = 0
+var totalLosses = 0
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var correctWorldLabel: UILabel!
@@ -14,12 +20,20 @@ class ViewController: UIViewController {
     @IBOutlet var lettterButton: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view.
+    }
+    
+    var currentGame: Game!
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord,incorrectMovesRemaining: incorrectMovesAllowed)
+
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
-        
-        
+        sender.isEnabled = false
     }
     
 }
---
+++ Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Daniel Apple Pie
+//
+//  Created by 7g on 10/18/22.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
