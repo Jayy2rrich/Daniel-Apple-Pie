dfd65b7a1f991716a7edf87a3bab2581d73e9bb4
Daniel <danielbonneau04@gmail.com>
2022-10-19 14:04:08 -0400

    10/19/22

--- Daniel Apple Pie/ViewController.swift
+++ Daniel Apple Pie/ViewController.swift
@@ -15,6 +15,7 @@ var totalLosses = 0
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWorldLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var lettterButton: [UIButton]!
@@ -27,13 +28,22 @@ class ViewController: UIViewController {
     var currentGame: Game!
     
     func newRound() {
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord,incorrectMovesRemaining: incorrectMovesAllowed)
+    let newWord = listOfWords.removeFirst()
+    currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed,guessedLetters: [])
+    updateUI()
+    }
+
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
 
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
+        let letterString = sender.configuration!.title
+        let letter = Character((letterString?.lowercased())!)
+
     }
     
 }
